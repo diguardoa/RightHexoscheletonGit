4e8471093476137f55dac06f2b6103a4571abf9b
Nicola Mastronicola <nicola.mastronicola@gmail.com>
2017-11-14 17:30:41 +0100

    fuzzy settings

--- Proxy/Connect.m
+++ Proxy/Connect.m
@@ -3,7 +3,12 @@ cutFreq = 0.1; %Hz
 sampleTime = 0.01;
 filt = tf(1,[cutFreq 1]);
 filt = c2d(filt,sampleTime);
-ampliFactor = 1.80;
+
+ampliFactor = 1.60;
+ampliFactorDistal = 1;
+
+% Set Hand Address
+RightHandAddress = '192.168.3.91';
 %% Connect to ROS
 
 % % Diego PC
@@ -18,8 +23,13 @@ ampliFactor = 1.80;
 
 % Virtual Machine
 % to connect 
-setenv('ROS_MASTER_URI','http://192.168.3.80:11311');
-setenv('ROS_IP','192.168.3.92');%'10.100.39.22');
+% setenv('ROS_MASTER_URI','http://192.168.3.80:11311');
+% setenv('ROS_IP','192.168.3.92');%'10.100.39.22');
+% rosinit
+
+%% Left Hand
+setenv('ROS_MASTER_URI','http://192.168.7.2:11311');
+setenv('ROS_IP','192.168.7.1');%'10.100.39.22');
 rosinit
 
 % setenv('ROS_MASTER_URI','http://192.168.56.102:11311');
